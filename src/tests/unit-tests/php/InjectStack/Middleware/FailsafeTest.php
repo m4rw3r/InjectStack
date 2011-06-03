2fc5d049508ea0d15a8f30f40a284abc25036bae
m4rw3r <m4rw3r@gmail.com>
2011-06-03 16:14:20 +0200

    Tests: Added unit tests for some Middlewares

---
+++ src/tests/unit-tests/php/InjectStack/Middleware/FailsafeTest.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * Created by Martin Wernståhl on 2011-03-06.
+ * Copyright (c) 2011 Martin Wernståhl.
+ * All rights reserved.
+ */
+
+namespace InjectStack\Middleware;
+
+use \InjectStack\MiddlewareInterface;
+
+class FailsafeTest extends \PHPUnit_Framework_TestCase
+{
+	public function testInstantiate()
+	{
+		$obj = new Failsafe();
+		
+		$this->assertTrue($obj instanceof Failsafe);
+		$this->assertTrue($obj instanceof MiddlewareInterface);
+	}
+	public function testRun()
+	{
+		$obj   = new Failsafe();
+		$param = false;
+		
+		$obj->setNext(function($env) use(&$param)
+		{
+			$param = func_get_args();
+			
+			return 'data';
+		});
+		
+		$this->assertEquals('data', $obj(array('test' => 'data')));
+		$this->assertEquals(array(array('test' => 'data')), $param);
+	}
+	public function testCatch()
+	{
+		$obj = new Failsafe();
+		
+		$obj->setNext(function()
+		{
+			throw new \Exception();
+		});
+		
+		$this->assertEquals(array(500, array(), ''), $obj(array()));
+	}
+}
+
+
+/* End of file FailsafeTest.php */
+/* Location: src/tests/unit-tests/php/InjectStack/Middleware */
---
+++ src/tests/unit-tests/php/InjectStack/Middleware/RunTimerTest.php
@@ -0,0 +1,93 @@
+<?php
+/*
+ * Created by Martin Wernståhl on 2011-03-06.
+ * Copyright (c) 2011 Martin Wernståhl.
+ * All rights reserved.
+ */
+
+namespace InjectStack\Middleware;
+
+use \InjectStack\MiddlewareInterface;
+
+class RunTimerTest extends \PHPUnit_Framework_TestCase
+{
+	public function testInstantiate()
+	{
+		$obj = new RunTimer();
+		
+		$this->assertTrue($obj instanceof RunTimer);
+		$this->assertTrue($obj instanceof MiddlewareInterface);
+	}
+	public function testRun()
+	{
+		$obj   = new RunTimer();
+		$param = false;
+		
+		$obj->setNext(function($env) use(&$param)
+		{
+			$param = func_get_args();
+			
+			return array(200, array(), 'data');
+		});
+		
+		$res = $obj(array('test' => 'data'));
+		
+		$this->assertXRuntimeKey($res, 'X-Runtime', 200, 'data');
+		
+		$this->assertEquals(array(array('test' => 'data')), $param);
+	}
+	
+	public function testRun2()
+	{
+		$obj   = new RunTimer('test');
+		$param = false;
+		
+		$obj->setNext(function($env) use(&$param)
+		{
+			$param = func_get_args();
+			
+			return array(200, array(), 'data');
+		});
+		
+		$res = $obj(array('test' => 'data'));
+		
+		$this->assertXRuntimeKey($res, 'X-Runtime-test', 200, 'data');
+		
+		$this->assertEquals(array(array('test' => 'data')), $param);
+	}
+	
+	public function testRu3()
+	{
+		$obj   = new RunTimer('FooBar');
+		$param = false;
+		
+		$obj->setNext(function($env) use(&$param)
+		{
+			$param = func_get_args();
+			
+			return array(200, array(), 'data');
+		});
+		
+		$res = $obj(array('test' => 'data'));
+		
+		$this->assertXRuntimeKey($res, 'X-Runtime-FooBar', 200, 'data');
+		
+		$this->assertEquals(array(array('test' => 'data')), $param);
+	}
+	
+	protected function assertXRuntimeKey($res, $name, $expected_ret_code, $expected_ret_data)
+	{
+		$this->assertEquals(3, count($res));
+		$this->assertArrayHasKey(0, $res);
+		$this->assertSame($expected_ret_code, $res[0]);
+		$this->assertArrayHasKey(1, $res);
+		$this->assertFalse(empty($res[1][$name]));
+		$this->assertInternalType('float', $res[1][$name]);
+		$this->assertArrayHasKey(2, $res);
+		$this->assertEquals($expected_ret_data, $res[2]);
+	}
+}
+
+
+/* End of file RunTimerTest.php */
+/* Location: src/tests/unit-tests/php/InjectStack/Middleware */
