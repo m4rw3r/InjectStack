a89fdb1695ffb2fbd7fb34f9a9f4b74b2d04e311
m4rw3r <m4rw3r@gmail.com>
2011-08-01 22:25:15 +0200

    Examples: Improved spacing in docco output for hello_world.php and MemcachedSessionCounter.php

--- examples/MemcachedSessionCounter.php
+++ examples/MemcachedSessionCounter.php
@@ -1,5 +1,4 @@
 <?php
-
 // This is a simple application which will count how many times a user has accessed
 // the application with the use of cookies and Memcached storage.
 
@@ -14,7 +13,6 @@ use \Inject\ClassTools\Autoloader\Generic as Autoloader;
 use \Inject\Stack\Builder;
 use \Inject\Stack\Adapter\Generic as ServerAdapter;
 use \Inject\Stack\Middleware;
-
 // Include the autoloader, make it load from the project's `src`-folder and instantiate it:
 require 'Inject/ClassTools/Autoloader/Generic.php';
 
@@ -22,12 +20,9 @@ set_include_path(realpath(__DIR__.'/../src/php').PATH_SEPARATOR.get_include_path
 
 $loader = new Autoloader();
 $loader->register();
-
 // Initialize Memcached to use with session middleware.
 $cache = new Memcached();
 $cache->addServer('127.0.0.1', 11211);
-
-
 // ## Stack Builder ##
 
 // The `Inject\Stack\Builder` is a helper class which will assemble
@@ -45,7 +40,7 @@ $stack = new Builder(
 		// Show exceptions nicely if we encounter any
 		// (`Session` can throw if there is an error connecting to Memcached):
 		new Middleware\ShowException(),
-		// The Session middleware which adds a session object to $env['inject.session']:
+		// The Session middleware which adds a session object to `$env['inject.session']`:
 		new Middleware\Session(
 			// Memcached storage adapter:
 			new Middleware\Session\MemcachedStorage($cache),
@@ -56,28 +51,22 @@ $stack = new Builder(
 		// (see PHPdoc for more information):
 		new Middleware\SessionInvalidator()
 	),
-	
 	// ## The Application ##
 	function($env)
 	{
-		// Check if we have a `count` entry in the session, if not fix one to zero:
+		// Check if we have a `count` entry in the session, if not set one to zero:
 		isset($env['inject.session']['count']) OR $env['inject.session']['count'] = 0;
-		
 		// Increase the count for the user:
 		$env['inject.session']['count']++;
-		
 		// Construct response:
 		$message  = 'Hello '.$env['inject.session']->getId()."!\n";
 		$message .= 'You have accessed this '.$env['inject.session']['count']." times.\n";
 		// We also dump the contents of the Session array:
 		$message .= print_r($env['inject.session']->getArrayCopy(), true);
-		
-		// Return 200 text response:
+		// Return 200 OK text response:
 		return array(200, array('Content-Type' => 'text/plain'), $message);
 	}
 );
-
-
 // ## Run Application ##
 $adapter = new ServerAdapter();
 $adapter->run($stack);
--- examples/hello_world.php
+++ examples/hello_world.php
@@ -1,9 +1,7 @@
 <?php
-
 // This is a simple Hello-World application written for Inject\Stack.
 // It will demonstrate basic bootstrapping and how to tie a simple
 // PHP Closure as the application.
-
 
 // ## Class loading ##
 
@@ -15,17 +13,13 @@ use \Inject\ClassTools\Autoloader\Generic as Autoloader;
 // This `Generic` adapter is meant to be run under a CGI-like environment,
 // like MOD_PHP or PHP-FPM.
 use \Inject\Stack\Adapter\Generic as ServerAdapter;
-
 // Add the project's source dir to the include path, in case we haven't been installed locally:
 set_include_path('../src/php'.PATH_SEPARATOR.get_include_path());
-
 // Include the autoloader and register it:
 include 'Inject/ClassTools/Autoloader/Generic.php';
 
 $loader = new Autoloader();
 $loader->register();
-
-
 // ## Hello World Application ##
 
 // Create the hello world application as a PHP Closure taking the
@@ -37,20 +31,16 @@ $hello_world = function($env)
 {
 	// Retreive name from GET parameters, if present, defaults to "World"
 	$name = empty($env['inject.get']['name']) ? 'World' : $env['inject.get']['name'];
-	
 	// Create the Response message:
 	$message = 'Hello '.$name.'!';
-	
 	// Return a 200 success text response:
 	return array(200, array('Content-Type' => 'text/plain'), $message);
 };
-
 // ## Run The Application ##
 
 // Instantiate the server adapter to handle the request and convert itto an
 // Inject\Stack compatible Environment hash.
 // It is also responsible for sending the response to the browser.
 $adapter = new ServerAdapter();
-
 // Run the application!
 $adapter->run($hello_world);
